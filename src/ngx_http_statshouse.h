6ce857127390115c5dd30f1454454e786ea0da61
Andrey Kolyshkin <a.kolyshkin@corp.vk.com>
2023-01-26 17:08:57 +0300

    Add api for the ability to send statistics from other modules

---
+++ src/ngx_http_statshouse.h
@@ -0,0 +1,18 @@
+/* Copyright 2022 V Kontakte LLC
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef _NGX_HTTP_STATSHOUSE_H_INCLUDED_
+#define _NGX_HTTP_STATSHOUSE_H_INCLUDED_
+
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+
+ngx_int_t  ngx_http_statshouse_send(ngx_http_request_t *request, ngx_str_t *phase);
+
+
+#endif
